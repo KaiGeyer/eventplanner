07f90e2e049e2a3bb8830005f0094abb922d7aea
KaiGeyer <k.geyer@email.de>
2026-09-08 10:38:27 +0200

    feat: Render events from data with map

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,38 +1,49 @@
 import EventCard from "./EventCard";
 
+const events = [
+    {
+    id: 1,
+    title: "React Grundlagen",
+    date: "12.September 2026",
+    location: "Raum 1",
+    speaker: "Anna Müller",
+    price: 129,
+    online: false,
+    },
+{
+    id:2,
+    title: "JavaScript Workshop",
+    date: "19. September 2026",
+    location: "Raum 2",
+    speaker: "Robert Rabe",
+    price: 149,
+    online: true,
+},
+{
+    id:3,
+    title: "CSS Styling",
+    date: "26. September 2026",
+    location: "Raum 3",
+    speaker: "Klausbern Kummer",
+    price: 177,
+    online: true, 
+}    
+
+
+]
+
 export default function EventList() {
     return(
         <section className="event-list">
-            <EventCard
-            title="React Grundlagen"
-            date="12.September 2026"
-            location="Raum 1"
-            speaker="Anna Müller"
-            price= { 129 }
-            online= { false }
-            />
+            { events.map( event => (
+                <EventCard
+                    key={event.id}
+                    {...event}
+                /> 
 
-            <EventCard
-            title="JavaScript Workshop"
-            date="19. September 2026"
-            location="Raum 2"
-            speaker="Robert Rabe"
-            price= { 149 }
-            online= { true }
-            />
-
-            <EventCard
-            title="CSS Styling"
-            date="26. September 2026"
-            location="Raum 3"
-            speaker="Klausbern Kummer"
-            price= { 177 }
-            online= { true }            
-            />
+            ))}
 
         </section>
-
-    )
-
+    );
 
 }
